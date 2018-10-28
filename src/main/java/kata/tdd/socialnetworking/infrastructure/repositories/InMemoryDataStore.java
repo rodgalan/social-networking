0d734815620fc3afa3833db62d3fe6c4d4f9e363
Anna Rodriguez Gallego <anna.rodriguez@scmspain.com>
2018-10-28 10:18:23 +0100

    A user can have more than one post: change 'should save a post' by 'should save two posts'

--- src/main/java/kata/tdd/socialnetworking/infrastructure/repositories/InMemoryDataStore.java
+++ src/main/java/kata/tdd/socialnetworking/infrastructure/repositories/InMemoryDataStore.java
@@ -2,18 +2,22 @@ package kata.tdd.socialnetworking.infrastructure.repositories;
 
 import kata.tdd.socialnetworking.domain.post.Post;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class InMemoryDataStore {
 
-  private final static Map<String, Post> dataStore = new HashMap<>();
+  private final static Map<String, List<Post>> dataStore = new HashMap<>();
 
-  static Post findBy(String username) {
+  static List<Post> findBy(String username) {
     return dataStore.get(username);
   }
 
   static void save(Post post) {
-    dataStore.put(post.getUsername(), post);
+    List<Post> userPostsList = dataStore.getOrDefault(post.getUsername(), new ArrayList<Post>());
+    userPostsList.add(post);
+    dataStore.put(post.getUsername(), userPostsList);
   }
 }
--- src/test/java/kata/tdd/socialnetworking/infrastructure/repositories/InMemoryPostRepositoryTest.java
+++ src/test/java/kata/tdd/socialnetworking/infrastructure/repositories/InMemoryPostRepositoryTest.java
@@ -2,6 +2,9 @@ package kata.tdd.socialnetworking.infrastructure.repositories;
 
 import kata.tdd.socialnetworking.domain.post.Post;
 import org.junit.Test;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class InMemoryPostRepositoryTest {
@@ -9,13 +12,17 @@ public class InMemoryPostRepositoryTest {
   private InMemoryPostRepository inMemoryPostRepository = new InMemoryPostRepository();
 
   @Test
-  public void should_save_a_post() {
-    Post post = new Post("Anna", "New Message");
+  public void should_save_two_posts() {
+    String username = "Maria";
+    Post post_1 = new Post(username, "New Message 1");
+    Post post_2 = new Post(username, "New Message 2");
 
-    inMemoryPostRepository.save(post);
+    inMemoryPostRepository.save(post_1);
+    inMemoryPostRepository.save(post_2);
 
-    Post expectedSavedPost = InMemoryDataStore.findBy(post.getUsername());
+    List<Post> expectedSavedPosts = InMemoryDataStore.findBy(username);
 
-    assertThat(post).isEqualTo(expectedSavedPost);
+    assertThat(expectedSavedPosts).containsExactlyInAnyOrder(post_1, post_2);
   }
+
 }
